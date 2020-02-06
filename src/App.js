b06e9eacc8e6d41c245af712e614915dad9f4184
sgatana <sgatana@gmail.com>
2020-02-06 10:19:12 +0300

    project set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import SignIn from './components/SignIn';
+import Layout from './components/Layout';
+import io from 'socket.io-client';
+import { USER_CONNECTED, LOGOUT } from './common/events';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = 'http://localhost:5000';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      socket: null
+    };
+  }
+
+  componentDidMount() {
+    this.initSocket();
+  }
+
+  // initialize connection
+  initSocket = () => {
+    const socket = io(url);
+    socket.on('connect', () => {
+    });
+    this.setState({ socket });
+  };
+
+  addUserToList = user => {
+    const { socket } = this.state;
+    socket.emit(USER_CONNECTED, user);
+    this.setState({ user });
+  };
+
+
+  handleSignOut = () => {
+    const { socket } = this.state;
+    socket.emit(LOGOUT);
+    this.setState({ user: null });
+  };
+
+  render() {
+    const { user, socket } = this.state;
+    return (
+      <div>
+        {!user ? (
+          <div className='account'>
+            <SignIn addUser={this.addUserToList} socket={socket} />
+          </div>
+        ) : (
+          <Layout
+            user={user}
+            socket={socket}
+            handleSignOut={this.handleSignOut}
+          />
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
